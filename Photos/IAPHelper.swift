01851052d45eb9718a3a11aee42604559375f465
Vasilica Costescu <vasilica.costescu@bgch.co.uk>
2015-02-17 19:37:58 +0000

    Retrieve list of products from iTunes Connect

---
+++ Photos/IAPHelper.swift
@@ -0,0 +1,53 @@
+//
+//  IAPHelper.swift
+//  Photos
+//
+//  Created by Vasilica Costescu on 17/02/2015.
+//  Copyright (c) 2015 Vasilica Costescu. All rights reserved.
+//
+
+import Foundation
+import StoreKit
+
+class IAPHelper: NSObject, SKProductsRequestDelegate {
+    
+    let productIdentifiers: NSSet
+    var completionHandler: ((Bool, [SKProduct]!) -> Void)!
+    
+    override init() {
+        
+        productIdentifiers = NSSet(objects: "NaturePhotos", "CatPhotos", "DogPhotos")
+    }
+    
+    func requestProductsWithCompletionHandler(completionHandler:(Bool, [SKProduct]!) -> Void){
+        self.completionHandler = completionHandler
+        
+        let productsRequest = SKProductsRequest(productIdentifiers: productIdentifiers)
+        productsRequest.delegate = self
+        productsRequest.start()
+    }
+    
+    //MARK: SKProductsRequestDelegate methods
+    func productsRequest(request: SKProductsRequest!, didReceiveResponse response: SKProductsResponse!) {
+        
+        println("product count \(response.products.count)")
+        println("invalid product IDs \(response.invalidProductIdentifiers)")
+        
+        if response.products.count > 0 {
+            var products: [SKProduct] = []
+            
+            for prod in response.products {
+                if prod.isKindOfClass(SKProduct) {
+                    products.append(prod as SKProduct)
+                }
+            }
+            
+            completionHandler(true, products)
+        }
+    }
+
+    func request(request: SKRequest!, didFailWithError error: NSError!) {
+        
+        completionHandler(false, nil)
+    }
+}
---
+++ Photos/MasterViewController.swift
@@ -0,0 +1,85 @@
+//
+//  MasterViewController.swift
+//  Photos
+//
+//  Created by Vasilica Costescu on 17/02/2015.
+//  Copyright (c) 2015 Vasilica Costescu. All rights reserved.
+//
+
+import UIKit
+import StoreKit
+
+class MasterViewController: UITableViewController {
+
+    var detailViewController: DetailViewController? = nil
+    var objects = NSMutableArray()
+    let helper = IAPHelper()
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+            self.clearsSelectionOnViewWillAppear = false
+            self.preferredContentSize = CGSize(width: 320.0, height: 600.0)
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        if let split = self.splitViewController {
+            let controllers = split.viewControllers
+            self.detailViewController = controllers[controllers.count-1].topViewController as? DetailViewController
+        }
+                        
+        helper.requestProductsWithCompletionHandler({ (success, products) -> Void in
+            if success {
+                self.objects.addObjectsFromArray(products)
+                self.tableView.reloadData()
+            } else {
+                let alert = UIAlertController(title: "Error", message: "Cannot retrieve products list right now.", preferredStyle: .Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            }
+        })
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Segues
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow() {
+                let product = objects[indexPath.row] as SKProduct
+                let controller = (segue.destinationViewController as UINavigationController).topViewController as DetailViewController
+                controller.detailItem = product
+                controller.title = product.localizedTitle
+                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+
+    // MARK: - Table View
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+
+        let product = objects[indexPath.row] as SKProduct
+        cell.textLabel!.text = product.localizedTitle
+        return cell
+    }
+}
+
